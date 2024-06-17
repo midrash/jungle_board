7c1675690aa1c6c516473982126c6b33579900fd
ParkJungHwan <pjh0740@naver.com>
2024-06-17 15:33:10 +0900

    배포 테스트 찐 마지막

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const auth = require("../auth")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('안녕하세오 테스트에오 이번엔 성공하나?');
+  res.send('안녕하세오 테스트에오 이번엔 성공하나? 진짜 진짜');
 });
 router.get('/test', function(req, res, next) {
   // res.render('index', { title: 'Express' });
