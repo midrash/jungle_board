d95ff53bc5eb1cf62f7c5af883d7a56a3a2339a4
ParkJungHwan <pjh0740@naver.com>
2024-06-19 16:47:58 +0900

    docker db 접속 prot 수정

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 var mysql = require("mysql2");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  // host     : 'host.docker.internal',
+  // host: "localhost",
+  host: "host.docker.internal",
   user: "root",
+  port: 3307,
   password: "root",
   database: "jgboard",
   dateStrings: "date",
