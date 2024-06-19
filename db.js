b78a101fc8567e25109856dc6bf6aa7e6d123a87
ParkJungHwan <pjh0740@naver.com>
2024-06-19 16:40:13 +0900

    db를 docker 로 변경 및 접속 루트 loclahost로 변경

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-var mysql      = require('mysql2');
+var mysql = require("mysql2");
 
 var connection = mysql.createConnection({
-  // host     : 'localhost',
-  host     : 'host.docker.internal',
-  user     : 'root',
-  password : 'root',
-  database : 'jgboard',
-  dateStrings: 'date' 
+  host: "localhost",
+  // host     : 'host.docker.internal',
+  user: "root",
+  password: "root",
+  database: "jgboard",
+  dateStrings: "date",
 });
- 
+
 connection.connect();
- 
+
 // connection.end();
 
 module.exports = {
   connection,
-}
+};
