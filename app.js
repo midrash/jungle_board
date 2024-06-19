5bb2e348fcea3a54098817d2b39a12d5c4a3fff9
ParkJungHwan <pjh0740@naver.com>
2024-06-19 18:33:41 +0900

    임시 저장

--- app.js
+++ app.js
@@ -1,52 +1,54 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+// var path = require('path');
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const cors = require("cors"); 
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var boardRouter = require('./routes/board');
-var commentRouter = require('./routes/comment');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var boardRouter = require("./routes/board");
+var commentRouter = require("./routes/comment");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "jade");
 
-app.use(cors({
-  origin: "*", // 접근 권한을 부여하는 도메인
-  // credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
-  // optionsSuccessStatus: 200, // 응답 상태 200으로 설정
-}));
+app.use(
+  cors({
+    origin: "*", // 접근 권한을 부여하는 도메인
+    // credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    // optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+  })
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/board', boardRouter);
-app.use('/comment', commentRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/board", boardRouter);
+app.use("/comment", commentRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
