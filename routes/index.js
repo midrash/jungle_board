61678b7b947e0017c7d87f19be11e02f89cba9e4
ParkJungHwan <pjh0740@naver.com>
2024-06-20 10:04:20 +0900

    테스트 배포

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const auth = require("../auth")
+const auth = require("../auth");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('합격 축하 ');
+  res.send("테스트 배포");
 });
-router.get('/test', function(req, res, next) {
+router.get("/test", function (req, res, next) {
   // res.render('index', { title: 'Express' });
-    console.log("토큰: "+ req.cookies.token);
-    auth.checkAuth(req.cookies.token,res);
-    res.send(req.cookies.token);
+  console.log("토큰: " + req.cookies.token);
+  auth.checkAuth(req.cookies.token, res);
+  res.send(req.cookies.token);
 });
 
 module.exports = router;
